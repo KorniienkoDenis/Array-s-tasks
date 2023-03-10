e105f7b5e34380a73aa6625e644973e9d62b3b44
Denis Korniienko <kubikrubikmegaminks@gmail.com>
2023-03-11 01:17:35 +0300

    done: task_3 completed

--- Task_3/Array's task 3/Array's task 3.cpp
+++ Task_3/Array's task 3/Array's task 3.cpp
@@ -1,20 +1,109 @@
-// Array's task 3.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <ctime>
+
+/*
+	
+	Task 3.
+		Write a binary search program for a key in the integer array.
+
+*/
+
+void InitializeArray(int arr[], const int size);
+void ShowArray(int arr[], const int size);
+void SortArray(int arr[], const int size);
+int BinarySearch(const int arr[], int left, int right, int key);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	srand(time(nullptr));
+
+	int key = 0;
+	const int size = 25;
+	int arr[size];
+
+	InitializeArray(arr, size);
+	ShowArray(arr, size);
+	std::cout << "-----------------------------------------------------------------------------------------------\n";
+	SortArray(arr, size);
+
+	std::cout << std::endl;
+	std::cout << "Enter number which you want to find in the array: ";
+	std::cin >> key;
+
+	int index = BinarySearch(arr, 0, size, key);
+
+	std::cout << std::endl;
+	std::cout << "You entered: " << key << std::endl;
+	
+	if (index >= 0)
+		std::cout << "Your number standing under index: " << index << std::endl;
+	else
+		std::cout << "Your number is not in the array!\n";
+
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void InitializeArray(int arr[], const int size)
+{
+	for (int index = 0; index < size; index++)
+		arr[index] = -25 + rand() % 50;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+	std::cout << std::endl;
+}
+
+void ShowArray(int arr[], const int size)
+{
+	std::cout << "Array: ";
+
+	for (int index = 0; index < size; index++)
+		std::cout << arr[index] << " ";
+
+	std::cout << std::endl;
+}
+
+void SortArray(int arr[], const int size)
+{
+	for (int index = 0; index < size; index++)
+	{
+		for (int j = index + 1; j < size; j++)
+		{
+			if (arr[j] < arr[index])
+			{
+				std::swap(arr[index], arr[j]);
+			}
+		}
+	}
+
+	std::cout << "Sorted the array: ";
+
+	for (int index = 0; index < size; index++)
+	{
+		std::cout << arr[index] << " ";
+	}
+
+	std::cout << std::endl;
+}
+
+int BinarySearch(const int arr[], int left, int right, int key)
+{
+	while (true)
+	{
+		int middle = (left + right) / 2;
+
+		if (key < arr[middle])
+		{
+			right = middle - 1;
+		}
+		else if (key > arr[middle])
+		{
+			left = middle + 1;
+		}
+		else
+			return middle;
+
+		if (left > right)
+			return -1;
+	}
+
+	std::cout << std::endl;
+}
