0bdce860ecc48984f217e795ad86119f10a56e05
Denis Korniienko <kubikrubikmegaminks@gmail.com>
2023-03-14 02:20:11 +0300

    done: task 7 completed

--- Task_7/Array's task 7/Array's task 7.cpp
+++ Task_7/Array's task 7/Array's task 7.cpp
@@ -1,20 +1,74 @@
-// Array's task 7.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <ctime>
+
+/*
+	
+	Task 7.
+		Find the least pair element in the array.
+
+*/
+
+void InitializeArray(int* arr, const int size);
+void ShowArray(int* arr, const int size);
+void ShowArrayWithPairElements(int* arr, const int size);
+int FindMinimumPairElement(int* arr, const int size);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	srand(time(nullptr));
+
+	const int size = 10;
+	int arr[size];
+
+	InitializeArray(arr, size);
+
+	ShowArray(arr, size);
+	ShowArrayWithPairElements(arr, size);
+
+	std::cout << "The least pair element: " << FindMinimumPairElement(arr, size) << "\n";
+
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void InitializeArray(int* arr, const int size)
+{
+	for (int index = 0; index < size; index++)
+		arr[index] = -5 + rand() % 15 + 1;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+	std::cout << std::endl;
+}
+
+void ShowArray(int* arr, const int size)
+{
+	std::cout << "Array: ";
+
+	for (int index = 0; index < size; index++)
+		std::cout << arr[index] << " ";
+	
+	std::cout << std::endl;
+}
+
+void ShowArrayWithPairElements(int* arr, const int size)
+{
+	std::cout << "The array with pair elements: ";
+
+	for (int index = 0; index < size; index++)
+		if (index % 2 == 0)
+			std::cout << arr[index] << " ";
+
+	std::cout << std::endl;
+}
+
+int FindMinimumPairElement(int* arr, const int size)
+{
+	int minimumPair = arr[0];
+
+	for (int index = 0; index < size; index++)
+		if (index % 2 == 0)
+			if (arr[index] < minimumPair)
+				minimumPair = arr[index];
+
+	std::cout << std::endl;
+
+	return minimumPair;
+}
